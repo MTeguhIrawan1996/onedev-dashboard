4d3bf6631da17ab9c37e2043e5ddf687e6072f7c
MTeguhIrawan1996 <teguhirawan1996@gmail.com>
2024-06-21 16:52:48 +0700

    :sparkles: feat(app): update entries PWA

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -23,7 +23,13 @@ const serwist = new Serwist({
   fallbacks: {
     entries: [
       {
-        url: '/offline', // the page that'll display if user goes offline
+        url: '/example', // the page that'll display if user goes offline
+        matcher({ request }) {
+          return request.destination === 'document';
+        },
+      },
+      {
+        url: '/overview', // the page that'll display if user goes offline
         matcher({ request }) {
           return request.destination === 'document';
         },
