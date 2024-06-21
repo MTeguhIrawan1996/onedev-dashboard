a682d2b3a325bfb1c55e4198d3701457846283e3
MTeguhIrawan1996 <teguhirawan1996@gmail.com>
2024-06-21 16:24:37 +0700

    :sparkles: feat(app): pWA Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { fileURLToPath } from 'node:url';
 import createJiti from 'jiti';
 const jiti = createJiti(fileURLToPath(import.meta.url));
@@ -11,12 +12,35 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
-});
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from 'next/constants.js';
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+export default async (phase) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+      missingSuspenseWithCSRBailout: false,
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import('@serwist/next')).default({
+      // Note: This is only an example. If you use Pages Router,
+      // use something else that works, such as "service-worker/index.ts".
+      swSrc: 'src/app/sw.ts',
+      swDest: 'public/sw.js',
+      reloadOnOnline: true,
+    });
+    return withBundleAnalyzer(withSerwist(nextConfig));
+  }
+
+  return nextConfig;
+};
